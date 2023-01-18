56042155c5307b4c3fa9fd252e2a1cbf35b5306e
Ankur Kumar <ank@leoank.me>
2023-01-18 10:31:11 +0530

    Add fs mol functions
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ src/compound_embedding/fs_mol/models/protonet.py
@@ -0,0 +1,223 @@
+from dataclasses import dataclass
+from typing import List, Tuple
+from typing_extensions import Literal
+
+import torch
+import torch.nn as nn
+
+from compound_embedding.fs_mol.data.protonet import ProtoNetBatch
+
+
+GRAPH_EMBEDDING_DIM = 5000
+FINGERPRINT_DIM = 2048
+PHYS_CHEM_DESCRIPTORS_DIM = 42
+
+
+@dataclass(frozen=True)
+class PrototypicalNetworkConfig:
+    # Model configuration:
+    used_features: Literal[
+        "gnn", "ecfp", "pc-descs", "gnn+ecfp", "ecfp+fc", "pc-descs+fc", "gnn+ecfp+pc-descs+fc"
+    ] = "gnn+ecfp+fc"
+    distance_metric: Literal["mahalanobis", "euclidean"] = "mahalanobis"
+
+
+class PrototypicalNetwork(nn.Module):
+    def __init__(self, config: PrototypicalNetworkConfig):
+        super().__init__()
+        self.config = config
+
+        self.use_fc = self.config.used_features.endswith("+fc")
+
+        # Create MLP if needed:
+        if self.use_fc:
+            # Determine dimension:
+            fc_in_dim = 0
+            if "gnn" in self.config.used_features:
+                fc_in_dim += GRAPH_EMBEDDING_DIM
+            if "ecfp" in self.config.used_features:
+                fc_in_dim += FINGERPRINT_DIM
+            if "pc-descs" in self.config.used_features:
+                fc_in_dim += PHYS_CHEM_DESCRIPTORS_DIM
+
+            self.fc = nn.Sequential(
+                nn.Linear(fc_in_dim, 1024),
+                nn.ReLU(),
+                nn.Linear(1024, 512),
+            )
+
+    @property
+    def device(self) -> torch.device:
+        return next(self.parameters()).device
+
+    def forward(self, input_batch: ProtoNetBatch):
+        support_features: List[torch.Tensor] = []
+        query_features: List[torch.Tensor] = []
+
+        if "gnn" in self.config.used_features:
+            support_features.append(input_batch.support_features.graph)
+            query_features.append(input_batch.query_features.graph)
+        if "ecfp" in self.config.used_features:
+            support_features.append(input_batch.support_features.fingerprints)
+            query_features.append(input_batch.query_features.fingerprints)
+        if "pc-descs" in self.config.used_features:
+            support_features.append(input_batch.support_features.descriptors)
+            query_features.append(input_batch.query_features.descriptors)
+
+        support_features_flat = torch.cat(support_features, dim=1)
+        query_features_flat = torch.cat(query_features, dim=1)
+
+        if self.use_fc:
+            support_features_flat = self.fc(support_features_flat)
+            query_features_flat = self.fc(query_features_flat)
+
+        if self.config.distance_metric == "mahalanobis":
+            class_means, class_precision_matrices = self.compute_class_means_and_precisions(
+                support_features_flat, input_batch.support_labels
+            )
+
+            # grabbing the number of classes and query examples for easier use later
+            number_of_classes = class_means.size(0)
+            number_of_targets = query_features_flat.size(0)
+
+            """
+            Calculating the Mahalanobis distance between query examples and the class means
+            including the class precision estimates in the calculations, reshaping the distances
+            and multiplying by -1 to produce the sample logits
+            """
+            repeated_target = query_features_flat.repeat(1, number_of_classes).view(
+                -1, class_means.size(1)
+            )
+            repeated_class_means = class_means.repeat(number_of_targets, 1)
+            repeated_difference = repeated_class_means - repeated_target
+            repeated_difference = repeated_difference.view(
+                number_of_targets, number_of_classes, repeated_difference.size(1)
+            ).permute(1, 0, 2)
+            first_half = torch.matmul(repeated_difference, class_precision_matrices)
+            logits = torch.mul(first_half, repeated_difference).sum(dim=2).transpose(1, 0) * -1
+        else:  # euclidean
+            logits = self._protonets_euclidean_classifier(
+                support_features_flat,
+                query_features_flat,
+                input_batch.support_labels,
+            )
+
+        return logits
+
+    def compute_class_means_and_precisions(
+        self, features: torch.Tensor, labels: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        means = []
+        precisions = []
+        task_covariance_estimate = self._estimate_cov(features)
+        for c in torch.unique(labels):
+            # filter out feature vectors which have class c
+            class_features = torch.index_select(features, 0, self._extract_class_indices(labels, c))
+            # mean pooling examples to form class means
+            means.append(torch.mean(class_features, dim=0, keepdim=True).squeeze())
+            lambda_k_tau = class_features.size(0) / (class_features.size(0) + 1)
+            lambda_k_tau = min(lambda_k_tau, 0.1)
+            precisions.append(
+                torch.inverse(
+                    (lambda_k_tau * self._estimate_cov(class_features))
+                    + ((1 - lambda_k_tau) * task_covariance_estimate)
+                    + 0.1
+                    * torch.eye(class_features.size(1), class_features.size(1)).to(self.device)
+                )
+            )
+
+        means = torch.stack(means)
+        precisions = torch.stack(precisions)
+
+        return means, precisions
+
+    @staticmethod
+    def _estimate_cov(
+        examples: torch.Tensor, rowvar: bool = False, inplace: bool = False
+    ) -> torch.Tensor:
+        """
+        SCM: Function based on the suggested implementation of Modar Tensai
+        and his answer as noted in:
+        https://discuss.pytorch.org/t/covariance-and-gradient-support/16217/5
+
+        Estimate a covariance matrix given data.
+
+        Covariance indicates the level to which two variables vary together.
+        If we examine N-dimensional samples, `X = [x_1, x_2, ... x_N]^T`,
+        then the covariance matrix element `C_{ij}` is the covariance of
+        `x_i` and `x_j`. The element `C_{ii}` is the variance of `x_i`.
+
+        Args:
+            examples: A 1-D or 2-D array containing multiple variables and observations.
+                Each row of `m` represents a variable, and each column a single
+                observation of all those variables.
+            rowvar: If `rowvar` is True, then each row represents a
+                variable, with observations in the columns. Otherwise, the
+                relationship is transposed: each column represents a variable,
+                while the rows contain observations.
+
+        Returns:
+            The covariance matrix of the variables.
+        """
+        if examples.dim() > 2:
+            raise ValueError("m has more than 2 dimensions")
+        if examples.dim() < 2:
+            examples = examples.view(1, -1)
+        if not rowvar and examples.size(0) != 1:
+            examples = examples.t()
+        factor = 1.0 / (examples.size(1) - 1)
+        if inplace:
+            examples -= torch.mean(examples, dim=1, keepdim=True)
+        else:
+            examples = examples - torch.mean(examples, dim=1, keepdim=True)
+        examples_t = examples.t()
+        return factor * examples.matmul(examples_t).squeeze()
+
+    @staticmethod
+    def _extract_class_indices(labels: torch.Tensor, which_class: torch.Tensor) -> torch.Tensor:
+        class_mask = torch.eq(labels, which_class)  # binary mask of labels equal to which_class
+        class_mask_indices = torch.nonzero(class_mask)  # indices of labels equal to which class
+        return torch.reshape(class_mask_indices, (-1,))  # reshape to be a 1D vector
+
+    @staticmethod
+    def compute_loss(logits: torch.Tensor, labels: torch.Tensor) -> torch.Tensor:
+        return nn.functional.cross_entropy(logits, labels.long())
+
+    def _protonets_euclidean_classifier(
+        self,
+        support_features: torch.Tensor,
+        query_features: torch.Tensor,
+        support_labels: torch.Tensor,
+    ) -> torch.Tensor:
+        class_prototypes = self._compute_class_prototypes(support_features, support_labels)
+        logits = self._euclidean_distances(query_features, class_prototypes)
+        return logits
+
+    def _compute_class_prototypes(
+        self, support_features: torch.Tensor, support_labels: torch.Tensor
+    ) -> torch.Tensor:
+        means = []
+        for c in torch.unique(support_labels):
+            # filter out feature vectors which have class c
+            class_features = torch.index_select(
+                support_features, 0, self._extract_class_indices(support_labels, c)
+            )
+            means.append(torch.mean(class_features, dim=0))
+        return torch.stack(means)
+
+    def _euclidean_distances(
+        self, query_features: torch.Tensor, class_prototypes: torch.Tensor
+    ) -> torch.Tensor:
+        num_query_features = query_features.shape[0]
+        num_prototypes = class_prototypes.shape[0]
+
+        distances = (
+            (
+                query_features.unsqueeze(1).expand(num_query_features, num_prototypes, -1)
+                - class_prototypes.unsqueeze(0).expand(num_query_features, num_prototypes, -1)
+            )
+            .pow(2)
+            .sum(dim=2)
+        )
+
+        return -distances
---
+++ src/compound_embedding/fs_mol/utils/test_utils.py
@@ -0,0 +1,255 @@
+import argparse
+import csv
+import dataclasses
+import tempfile
+import json
+import logging
+import os
+import time
+from dataclasses import dataclass
+from typing import Callable, Dict, Iterable, List, Optional, Tuple, Union
+
+from dpu_utils.utils.richpath import RichPath
+
+from fs_mol.data.fsmol_dataset import DataFold, FSMolDataset
+from fs_mol.data.fsmol_task import FSMolTask, FSMolTaskSample
+from fs_mol.data.fsmol_task_sampler import (
+    DatasetClassTooSmallException,
+    DatasetTooSmallException,
+    FoldTooSmallException,
+    StratifiedTaskSampler,
+)
+from fs_mol.utils.cli_utils import set_seed
+from fs_mol.utils.logging import prefix_log_msgs, set_up_logging
+from fs_mol.utils.metrics import BinaryEvalMetrics
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class FSMolTaskSampleEvalResults(BinaryEvalMetrics):
+    task_name: str
+    seed: int
+    num_train: int
+    num_test: int
+    fraction_pos_train: float
+    fraction_pos_test: float
+
+
+def add_data_cli_args(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument(
+        "DATA_PATH",
+        type=str,
+        nargs="+",
+        help=(
+            "File(s) containing the test data."
+            " If this is a directory, the --task-list-file argument will be used to determine what files to test on."
+            " Otherwise, it is the data file(s) on which testing is done."
+        ),
+    )
+
+    parser.add_argument(
+        "--task-list-file",
+        default="datasets/fsmol-0.1.json",
+        type=str,
+        help=("JSON file containing the lists of tasks to be used in training/test/valid splits."),
+    )
+
+
+def add_eval_cli_args(parser: argparse.ArgumentParser) -> None:
+    add_data_cli_args(parser)
+
+    parser.add_argument(
+        "--save-dir",
+        type=str,
+        default="outputs",
+        help="Path in which to store the test results and log of their computation.",
+    )
+
+    parser.add_argument(
+        "--num-runs", type=int, default=10, help="Number of runs with different data splits to do."
+    )
+
+    parser.add_argument("--seed", type=int, default=0, help="Random seed to use.")
+
+    parser.add_argument(
+        "--train-sizes",
+        type=json.loads,
+        default=[16, 32, 64, 128, 256],
+        help="JSON list of number of training points to sample.",
+    )
+
+    parser.add_argument(
+        "--test-size",
+        type=int,
+        default=None,
+        help="Number of test samples to take, default is take all remaining after splitting out train.",
+    )
+
+
+def set_up_dataset(args: argparse.Namespace, **kwargs):
+    # Handle the different task entry methods.
+    # Permit a directory or a list of files
+    if len(args.DATA_PATH) == 1 and RichPath.create(args.DATA_PATH[0]).is_dir():
+        assert (
+            RichPath.create(args.DATA_PATH[0]).join("test").exists()
+        ), "If DATA_PATH is a directory it must contain test/ sub-directory for evaluation."
+
+        return FSMolDataset.from_directory(
+            args.DATA_PATH[0], task_list_file=RichPath.create(args.task_list_file), **kwargs
+        )
+    else:
+        return FSMolDataset(test_data_paths=[RichPath.create(p) for p in args.DATA_PATH], **kwargs)
+
+
+def set_up_test_run(
+    model_name: str, args: argparse.Namespace, torch: bool = False, tf: bool = False
+) -> Tuple[str, FSMolDataset]:
+    set_seed(args.seed, torch=torch, tf=tf)
+    run_name = f"FSMol_Eval_{model_name}_{time.strftime('%Y-%m-%d_%H-%M-%S')}"
+    out_dir = os.path.join(args.save_dir, run_name)
+    os.makedirs(out_dir, exist_ok=True)
+    set_up_logging(os.path.join(out_dir, f"{run_name}.log"))
+
+    dataset = set_up_dataset(args)
+    logger.info(
+        f"Starting test run {run_name} on {len(dataset.get_task_names(DataFold.TEST))} assays"
+    )
+    logger.info(f"\tArguments: {args}")
+    logger.info(f"\tOutput dir: {out_dir}")
+
+    return out_dir, dataset
+
+
+def write_csv_summary(output_csv_file: str, test_results: Iterable[FSMolTaskSampleEvalResults]):
+    with open(output_csv_file, "w", newline="") as csv_file:
+        fieldnames = [
+            "num_train_requested",
+            "num_train",
+            "fraction_positive_train",
+            "num_test",
+            "fraction_positive_test",
+            "seed",
+            "valid_score",
+            "average_precision_score",
+            "roc_auc",
+            "acc",
+            "balanced_acc",
+            "precision",
+            "recall",
+            "f1_score",
+            "delta_auprc",
+        ]
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+
+        for test_result in test_results:
+            csv_writer.writerow(
+                {
+                    "num_train_requested": test_result.num_train,
+                    "num_train": test_result.num_train,
+                    "num_test": test_result.num_test,
+                    "fraction_positive_train": test_result.fraction_pos_train,
+                    "fraction_positive_test": test_result.fraction_pos_test,
+                    "seed": test_result.seed,
+                    "average_precision_score": test_result.avg_precision,
+                    "roc_auc": test_result.roc_auc,
+                    "acc": test_result.acc,
+                    "balanced_acc": test_result.balanced_acc,
+                    "precision": test_result.prec,
+                    "recall": test_result.recall,
+                    "f1_score": test_result.f1,
+                    "delta_auprc": test_result.avg_precision - test_result.fraction_pos_test,
+                }
+            )
+
+
+def eval_model(
+    test_model_fn: Callable[[FSMolTaskSample, str, int], BinaryEvalMetrics],
+    dataset: FSMolDataset,
+    train_set_sample_sizes: List[int],
+    out_dir: Optional[str] = None,
+    num_samples: int = 10,
+    valid_size_or_ratio: Union[int, float] = 0.0,
+    test_size_or_ratio: Optional[Union[int, float, Tuple[int, int]]] = None,
+    fold: DataFold = DataFold.TEST,
+    task_reader_fn: Optional[Callable[[List[RichPath], int], Iterable[FSMolTask]]] = None,
+    seed: int = 0,
+) -> Dict[str, List[FSMolTaskSampleEvalResults]]:
+    """Evaluate a model on the FSMolDataset passed.
+
+    Args:
+        test_model_fn: A callable directly evaluating the model of interest on a single task
+            sample in the form of an FSMolTaskSample. The test_model_fn should act on the task
+            sample with the model, using a temporary output folder and seed. All other required
+            variables should be defined in the same context as the callable. The function should
+            return a BinaryEvalMetrics object from the task.
+        dataset: An FSMolDataset with paths to the data to be evaluated supplied.
+        train_set_samples_sizes: List[int], a list of the support set sizes at which to evaluate,
+            this is the train_samples size in a TaskSample.
+        out_dir: final output directory for evaluation results.
+        num_samples: number of repeated draws from the task's data on which to evaluate the model.
+        valid_size_or_ratio: size of validation set in a TaskSample.
+        test_size_or_ratio: size of the test set in a TaskSample.
+        fold: the fold of FSMolDataset on which to perform evaluation, typically will be the test fold.
+        task_reader_fn: Callable allowing additional transformations on the data prior to its batching
+            and passing through a model.
+        seed: an base external seed value. Repeated runs vary from this seed.
+    """
+    task_reading_kwargs = {"task_reader_fn": task_reader_fn} if task_reader_fn is not None else {}
+    task_to_results: Dict[str, List[FSMolTaskSampleEvalResults]] = {}
+
+    for task in dataset.get_task_reading_iterable(fold, **task_reading_kwargs):
+        test_results: List[FSMolTaskSampleEvalResults] = []
+        for train_size in train_set_sample_sizes:
+            task_sampler = StratifiedTaskSampler(
+                train_size_or_ratio=train_size,
+                valid_size_or_ratio=valid_size_or_ratio,
+                test_size_or_ratio=test_size_or_ratio,
+                allow_smaller_test=True,
+            )
+
+            for run_idx in range(num_samples):
+                logger.info(f"=== Evaluating on {task.name}, #train {train_size}, run {run_idx}")
+                with prefix_log_msgs(
+                    f" Test - Task {task.name} - Size {train_size:3d} - Run {run_idx}"
+                ), tempfile.TemporaryDirectory() as temp_out_folder:
+                    local_seed = seed + run_idx
+
+                    try:
+                        task_sample = task_sampler.sample(task, seed=local_seed)
+                    except (
+                        DatasetTooSmallException,
+                        DatasetClassTooSmallException,
+                        FoldTooSmallException,
+                        ValueError,
+                    ) as e:
+                        logger.debug(
+                            f"Failed to draw sample with {train_size} train points for {task.name}. Skipping."
+                        )
+                        logger.debug("Sampling error: " + str(e))
+                        continue
+
+                    test_metrics = test_model_fn(task_sample, temp_out_folder, local_seed)
+
+                    test_results.append(
+                        FSMolTaskSampleEvalResults(
+                            task_name=task.name,
+                            seed=local_seed,
+                            num_train=train_size,
+                            num_test=len(task_sample.test_samples),
+                            fraction_pos_train=task_sample.train_pos_label_ratio,
+                            fraction_pos_test=task_sample.test_pos_label_ratio,
+                            **dataclasses.asdict(test_metrics),
+                        )
+                    )
+
+        task_to_results[task.name] = test_results
+
+        if out_dir is not None:
+            write_csv_summary(os.path.join(out_dir, f"{task.name}_eval_results.csv"), test_results)
+
+    logger.info("=== Completed evaluation on all tasks.")
+
+    return task_to_results
