81e7691b99e58bee12582ae5f1e10a352dc2d910
Ankur Kumar <ank@leoank.me>
2023-02-09 17:11:03 +0100

    Update control flow of qc cli

--- src/compound_embedding/cli/main.py
+++ src/compound_embedding/cli/main.py
@@ -3,11 +3,15 @@
 import csv
 from itertools import chain
 from pathlib import Path
+import os
+import sys
 
 import click
+import joblib
 from joblib import cpu_count
 
 from compound_embedding.cli.generate import gen
+from compound_embedding.cli.test import test
 from compound_embedding.cli.train import train
 from compound_embedding.pipelines.common import get_all_paths, parallel_on_generic
 from compound_embedding.pipelines.data_qc import (
@@ -27,16 +31,25 @@ from .. import __version__
 @click.option("--jobs", type=int, help="Number of threads to use", default=cpu_count())
 def qc(inp: str, out: str, seq: bool, fix: bool, jobs: int) -> None:
     """Run data quality check."""
-    file_paths = get_all_paths(Path(inp))
-    print(f"Number of input files found: {len(file_paths)}")
-    if seq:
-        corrupted_file_paths = check_mol_counts(Path(inp), Path(out))
+    saved_corrupted_files_array = Path().joinpath("corrupted_files.joblib")
+    if saved_corrupted_files_array.is_file():
+        corrupted_file_paths = joblib.load(saved_corrupted_files_array)
     else:
-        corrupted_file_paths = parallel_on_generic(
-            file_paths, check_mol_counts, [Path(out)], jobs
-        )
-        corrupted_file_paths = list(chain.from_iterable(corrupted_file_paths))
-
+        file_paths = get_all_paths(Path(inp))
+        print(f"Number of input files found: {len(file_paths)}")
+        if seq:
+            corrupted_file_paths, corrupted_file_reasons = check_mol_counts(file_paths, Path(out))
+        else:
+            worker_out = parallel_on_generic(
+                file_paths, check_mol_counts, [Path(out)], cpu_count()
+            )
+            corrupted_file_paths = [tup[0] for tup in worker_out]
+            corrupted_file_reasons = [tup[1] for tup in worker_out]
+            corrupted_file_paths = list(chain.from_iterable(corrupted_file_paths))
+            corrupted_file_reasons = list(chain.from_iterable(corrupted_file_reasons))
+            joblib.dump(corrupted_file_paths, Path().joinpath("corrupted_files.joblib"))
+            joblib.dump(corrupted_file_reasons, Path().joinpath("corrupted_file_reasons.joblib"))
+    
     if fix:
         print(f"Found {len(corrupted_file_paths)} to fix.")
         # delete part files
@@ -49,9 +62,14 @@ def qc(inp: str, out: str, seq: bool, fix: bool, jobs: int) -> None:
             parallel_on_generic(
                 corrupted_file_paths,
                 fix_corrupted_files,
-                [Path(out), ["grover", "mordred"]],
+                [Path(inp), ["grover", "mordred"]],
                 jobs,
             )
+            # Delete dumped corrupted files array
+            if saved_corrupted_files_array.is_file():
+                saved_corrupted_files_array.unlink()
+            
+            # Return early - do not save corrupted files list as csv
             return
 
     # Write corrupted files to disk
@@ -71,4 +89,5 @@ def main() -> None:
 
 main.add_command(gen)
 main.add_command(train)
+main.add_command(test)
 main.add_command(qc)
