d7b45f68e6ae099668e27eeb8503e70f9514c76b
Ankur Kumar <ank@leoank.me>
2023-02-22 04:45:04 +0100

    Update train cli for EFP training

--- src/compound_embedding/cli/train.py
+++ src/compound_embedding/cli/train.py
@@ -6,7 +6,10 @@ from pathlib import Path
 
 import click
 from dpu_utils.utils import RichPath
+import pytorch_lightning as pl
+from pytorch_lightning import loggers as pl_loggers
 import torch
+from torch.utils.data import DataLoader
 
 from compound_embedding.fs_mol.data.fsmol_dataset import FSMolDataset
 from compound_embedding.fs_mol.utils.protonet_utils import (
@@ -14,13 +17,14 @@ from compound_embedding.fs_mol.utils.protonet_utils import (
     PrototypicalNetworkTrainer,
 )
 from compound_embedding.fs_mol.utils.cli_utils import set_seed
+from compound_embedding.models import ErsiliaFingerprint, build_datapipe
 
 
 @click.command()
 @click.option(
     "--save_dir", type=str, help="Directory to save model and run info.", required=True
 )
-@click.option("--data_dir", type=str, help="Dataset directory.", required=True)
+@click.option("--data_file", type=str, help="Dataset directory.", required=True)
 @click.option(
     "--features",
     type=click.Choice(["ecfp+grover+mordred+fc", "ecfp+grover+fc"]),
@@ -79,7 +83,7 @@ def protonet(
         tasks_per_batch=tasks_per_batch,
         support_set_size=support_set_size,
         query_set_size=query_set_size,
-        validate_every_num_steps=50,
+        validate_every_num_steps=10,
         validation_support_set_sizes=tuple([16, 128]),
         validation_query_set_size=512,
         validation_num_samples=5,
@@ -91,7 +95,9 @@ def protonet(
     set_seed(seed, True, False)
 
     # load dataset
-    dataset = FSMolDataset.from_directory(directory=RichPath.create(data_dir))
+    dataset = FSMolDataset.from_directory(
+        directory=RichPath.create(data_dir), num_workers=5
+    )
 
     run_name = f"FSMol_protonet_{time.strftime('%Y-%m-%d_%H-%M-%S')}"
     out_dir = Path(save_dir).joinpath(run_name)
@@ -111,7 +117,7 @@ def protonet(
 @click.option(
     "--save_dir", type=str, help="Directory to save model and run info.", required=True
 )
-@click.option("--data_dir", type=str, help="Dataset directory.", required=True)
+@click.option("--data_file", type=str, help="Dataset hdf5 file.", required=True)
 @click.option(
     "--batch_size", type=int, default=256, help="Number of examples per batch."
 )
@@ -122,7 +128,7 @@ def protonet(
 @click.option("--seed", type=int, help="Set random seed.", default=42)
 def efp(
     save_dir: str,
-    data_dir: str,
+    data_file: str,
     batch_size: int,
     num_train_steps: int,
     lr: float,
@@ -131,14 +137,37 @@ def efp(
     set_seed(seed, True, False)
     # load dataset
 
-    run_name = f"FSMol_protonet_{time.strftime('%Y-%m-%d_%H-%M-%S')}"
+    run_name = f"EFP_Train_{time.strftime('%Y-%m-%d_%H-%M-%S')}"
     out_dir = Path(save_dir).joinpath(run_name)
     os.makedirs(out_dir, exist_ok=True)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = {}
-    model.to(device)
-    model.train()
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        dirpath=str(out_dir),
+        save_top_k=3,
+        monitor="VAL_Loss",
+    )
+    tb_logger = pl_loggers.TensorBoardLogger(save_dir=str(out_dir.joinpath("logs")))
+    trainer = pl.Trainer(
+        callbacks=[checkpoint_callback],
+        accelerator="auto",
+        val_check_interval=200,
+        max_epochs=num_train_steps,
+        log_every_n_steps=1,
+        logger=tb_logger,
+    )
+
+    model = ErsiliaFingerprint()
+
+    train_datapipe = build_datapipe([0, 1399565], data_file, ["morgan", "protonet"])
+    val_datapipe = build_datapipe([1399565, 1399885], data_file, ["morgan", "protonet"])
+    train_loader = DataLoader(train_datapipe, batch_size, num_workers=12)
+    val_loader = DataLoader(val_datapipe, batch_size, num_workers=12)
+    print("Starting training...")
+    trainer.fit(
+        model=model,
+        train_dataloaders=train_loader,
+        val_dataloaders=val_loader,
+    )
 
 
 @click.group()
