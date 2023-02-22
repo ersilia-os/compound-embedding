3fdaa90ffe404e0e0b88ea0d9257ca9e16d497b6
Ankur Kumar <ank@leoank.me>
2023-02-22 12:38:54 +0100

    Add model
    Signed-off-by: Ankur Kumar <ank@leoank.me>

---
+++ lite/src/eosce/models.py
@@ -0,0 +1,36 @@
+"""Models."""
+
+from typing import List, Optional
+from pathlib import Path
+
+import numpy as np
+import onnxruntime as ort
+
+from eosce.utils import get_package_root_path, smiles_to_morgan
+
+class ErsiliaCompoundEmbeddings:
+    def __init__(self: "ErsiliaCompoundEmbeddings", onnx_path: Optional[Path] = get_package_root_path().joinpath("efp.onnx")) -> None:
+        """Initialize Ersilia compound embeddings model.
+
+        Args:
+            onnx_path (Optional[Path], optional): Path to model onnx file.
+                Defaults to get_package_root_path().joinpath("efp.onnx").
+        """
+        
+        self.providers = ort.get_available_providers()
+        print(f"Available providers: {self.providers}")
+        self.session = ort.InferenceSession(onnx_path, provider_options=self.providers)
+
+    def transform(self: "ErsiliaCompoundEmbeddings", smiles: List[str]) -> List[np.ndarray]:
+        """Transform smiles to embeddings.
+
+        Args:
+            smiles (List[str]): A list of smile strings.
+
+        Returns:
+            List[np.ndarray]: A list of embeddings.
+        """
+        # Convert smiles to morgan
+        morgan = smiles_to_morgan(smiles)
+        output = self.session.run(["embeddings_list"], {"smiles_list": morgan})
+        return output
