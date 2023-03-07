ac0a7e87d50a5a02d2462fc65e9bfa71e292454e
Ankur Kumar <ank@leoank.me>
2023-03-07 16:37:21 +0100

    Remove dep on griddify
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- lite/src/eosce/models.py
+++ lite/src/eosce/models.py
@@ -8,10 +8,12 @@ import joblib
 import numpy as np
 import onnxruntime as ort
 
-from eosce.utils import get_package_root_path, smiles_to_morgan
+from eosce.utils import Flat2Grid, get_package_root_path, smiles_to_morgan
 
 
-def get_ort_session(onnx_path: Path, providers: Optional[List[str]] = ort.get_available_providers()) -> Any:
+def get_ort_session(
+    onnx_path: Path, providers: Optional[List[str]] = ort.get_available_providers()
+) -> Any:
     """Create an inference session with working provider.
 
     Args:
@@ -31,7 +33,10 @@ def get_ort_session(onnx_path: Path, providers: Optional[List[str]] = ort.get_av
 
 
 class ErsiliaCompoundEmbeddings:
-    def __init__(self: "ErsiliaCompoundEmbeddings", onnx_path: Optional[Path] = get_package_root_path().joinpath("efp.onnx")) -> None:
+    def __init__(
+        self: "ErsiliaCompoundEmbeddings",
+        onnx_path: Optional[Path] = get_package_root_path().joinpath("efp.onnx"),
+    ) -> None:
         """Initialize Ersilia compound embeddings model.
 
         Args:
@@ -39,9 +44,12 @@ class ErsiliaCompoundEmbeddings:
                 Defaults to get_package_root_path().joinpath("efp.onnx").
         """
         self.session = get_ort_session(onnx_path)
-        self.griddify = joblib.load(get_package_root_path() / "griddify.joblib")
+        self.mappings = joblib.load(get_package_root_path() / "mappings.joblib")
+        self.griddify = Flat2Grid(self.mappings, 32)
 
-    def transform(self: "ErsiliaCompoundEmbeddings", smiles: List[str], grid: bool = False) -> np.ndarray:
+    def transform(
+        self: "ErsiliaCompoundEmbeddings", smiles: List[str], grid: bool = False
+    ) -> np.ndarray:
         """Transform smiles to embeddings.
 
         Args:
@@ -53,7 +61,10 @@ class ErsiliaCompoundEmbeddings:
         """
         # Convert smiles to morgan
         morgan_list = smiles_to_morgan(smiles)
-        embedding_array = self.session.run(["embedding_list"], {"morgan_list": np.asarray(morgan_list, dtype=np.float32)})[0]
+        embedding_array = self.session.run(
+            ["embedding_list"],
+            {"morgan_list": np.asarray(morgan_list, dtype=np.float32)},
+        )[0]
         if grid:
             # griddify the embedding
             return self.griddify.transform(embedding_array)
--- lite/src/eosce/utils.py
+++ lite/src/eosce/utils.py
@@ -7,6 +7,7 @@ import numpy as np
 from rdkit import DataStructs
 from rdkit.Chem import MolFromSmiles, rdFingerprintGenerator
 
+
 def get_package_root_path() -> Path:
     """Get path of the package root.
 
@@ -14,6 +15,7 @@ def get_package_root_path() -> Path:
         Path: Package root path
     """
     return Path(__file__).parent.absolute()
+
 
 def smiles_to_morgan(smiles: List[str]) -> List[np.ndarray]:
     """Convert smiles to Morgan fingerprints."""
@@ -26,3 +28,23 @@ def smiles_to_morgan(smiles: List[str]) -> List[np.ndarray]:
         DataStructs.ConvertToNumpyArray(fingerprint_vect, fingerprint)
         fingerprints.append(fingerprint)
     return np.array(fingerprints)
+
+
+#  Adapted from https://github.com/ersilia-os/griddify
+class Flat2Grid(object):
+    def __init__(self, mappings, side):
+        self._mappings = mappings
+        self._side = side
+
+    def transform(self, X):
+        X = np.array(X)
+        Xt_sum = np.zeros((X.shape[0], self._side, self._side))
+        Xt_cnt = np.zeros(Xt_sum.shape, dtype=int)
+        for i in range(X.shape[0]):
+            x = X[i, :]
+            for j, v in enumerate(x):
+                idx_i, idx_j = self._mappings[j]
+                Xt_sum[i, idx_i, idx_j] += v
+                Xt_cnt[i, idx_i, idx_j] += 1
+        Xt = Xt_sum / Xt_cnt
+        return Xt
