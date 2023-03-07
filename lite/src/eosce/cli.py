d3a0b42c22c3998020a17dd31d881848608110b6
Ankur Kumar <ank@leoank.me>
2023-03-07 16:38:45 +0100

    Update cli to support csv input and output
    Closes Passing input files #1
    
    Signed-off-by: Ankur Kumar <ank@leoank.me>

--- lite/src/eosce/cli.py
+++ lite/src/eosce/cli.py
@@ -1,6 +1,8 @@
 """Ersilia embeddings cli."""
 
+import csv
 import logging
+from pathlib import Path
 from typing import List, Optional
 
 import click
@@ -9,27 +11,55 @@ import numpy as np
 from . import __version__
 from eosce.models import ErsiliaCompoundEmbeddings
 
+
 @click.command()
 @click.option("--debug", is_flag=True, help="Ouput debug logs.")
 @click.option("--grid", is_flag=True, help="Convert embeddings to a grid.")
-@click.option("--out", type=str, help="File path to save results.")
+@click.option("-i", "--inp", type=str, help="CSV path to read input.")
+@click.option(
+    "--out",
+    type=str,
+    help="File path to save results. Extension of the file is used to select the output format.",
+)
 @click.argument("SMILES", nargs=-1)
-def embed(debug: bool, grid: bool, out: Optional[str], smiles: List[str]) -> None:
+def embed(
+    debug: bool, grid: bool, inp: Optional[str], out: Optional[str], smiles: List[str]
+) -> None:
     """Generate embeddings."""
     if debug:
         logging.basicConfig(level=logging.DEBUG)
     model = ErsiliaCompoundEmbeddings()
+
+    # Parse input csv if given
+    if inp:
+        csv_path = Path(inp)
+        with csv_path.open() as f:
+            file = csv.reader(f)
+            smiles = [line[0] for line in file]
     embeddings = model.transform(smiles, grid)
+
+    # Parse output format requested
     if out:
-        np.savez_compressed(out, embeddings=embeddings)
+        ext = out.split(".")[-1]
+        if ext == "csv":
+            with open(out, "w", newline="") as f:
+                writer = csv.writer(f)
+                for i, row in enumerate(embeddings):
+                    writer.writerow([smiles[i], *row])
+        elif ext == "npz":
+            np.savez_compressed(out, embeddings=embeddings)
+        else:
+            raise Exception(f"File format {ext} is not supported for saving.")
     else:
         np.set_printoptions(threshold=np.inf)
         print(embeddings)
+
 
 @click.group()
 @click.version_option(version=__version__)
 def main() -> None:
     """Ersilia embeddings CLI."""
     pass
+
 
 main.add_command(embed)
